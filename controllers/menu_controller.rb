e240517153b15da3a6ec2d94173e505aa2eb59ee
Victoria Fung <victoriahkfung@gmail.com>
2016-11-11 22:47:42 -0800

    view specific entry method added

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_n
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -51,9 +56,24 @@ class MenuController
       puts entry.to_s
       entry_submenu(entry)
     end
-    
+
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry_n
+    print "Input entry number: "
+    num = gets.chomp.to_i
+
+    if num < address_book.entries.count
+      puts address_book.entries[num-1]
+      puts "Press enter to return to main menu"
+      gets.chomp
+      system "clear"
+    else
+      print "#{num} is an invalid input\n"
+      view_entry_n
+    end
   end
 
   def create_entry
